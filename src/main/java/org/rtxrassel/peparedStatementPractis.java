5631d5abbc857f89449daf4d47a4aa9c13e91174
deveploper99 <rtxdeveploper99@gmail.com>
2026-03-05 22:36:01 +0600

    viwe all data

--- src/main/java/org/rtxrassel/peparedStatementPractis.java
+++ src/main/java/org/rtxrassel/peparedStatementPractis.java
@@ -14,17 +14,31 @@ public class peparedStatementPractis {
             Connection con = DriverManager.getConnection(url,userName,password);
             System.out.println("Connection Successfully");
             PreparedStatement pstm = con.prepareStatement(query);
-           pstm.setString(1,"RasselHassan");
-           pstm.setInt(2,7);
-           pstm.setString(3,"rtx@gmail.com");
-           pstm.setString(4,"0918327366");
-           pstm.setDouble(5,4.33);
+            pstm.setString(1,"RasselHassan");
+            pstm.setInt(2,7);
+            pstm.setString(3,"rtx@gmail.com");
+            pstm.setString(4,"0918327366");
+            pstm.setDouble(5,4.33);
 
            int rowsAffected = pstm.executeUpdate();
 
            if (rowsAffected>0){
                System.out.println("data insert Successfully");
            }else System.out.println("data not insert failed");
+
+
+            String selectQuery = "SELECT * FROM students";
+            ResultSet result = pstm.executeQuery(selectQuery);
+            while (result.next()){
+                int id = result.getInt("id");
+                String name = result.getString("name");
+                int roll = result.getInt("roll");
+                String email = result.getString("email");
+                String phone = result.getString("phone");
+                double cgpa = result.getDouble("cgpa");
+
+                System.out.println(id+" | "+name+" | "+roll+" | "+email+" | "+phone+" | "+cgpa);
+            }
 
         }catch (SQLException e){
             System.out.println(e.getMessage());
